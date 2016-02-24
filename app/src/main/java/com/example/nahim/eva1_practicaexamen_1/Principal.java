2135cca4a1d2fac39abf7adeee836abfe1ae5283
NahimRgz <nahimrgz@ingenieros.com>
2016-02-23 23:43:05 -0700

    radiobutton plumboxing

--- app/src/main/java/com/example/nahim/eva1_practicaexamen_1/Principal.java
+++ app/src/main/java/com/example/nahim/eva1_practicaexamen_1/Principal.java
@@ -1,13 +1,15 @@
 package com.example.nahim.eva1_practicaexamen_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 
 public class Principal extends AppCompatActivity {
-
+    RadioGroup rdgCafe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        rdgCafe = (RadioGroup) findViewById(R.id.rdgCafe);
     }
 }
