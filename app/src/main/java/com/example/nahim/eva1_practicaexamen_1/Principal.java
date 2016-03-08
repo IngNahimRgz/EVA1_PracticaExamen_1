9c49cdabbe2b099765756f1b7d5738778c50a7e8
NahimRgz <nahimrgz@ingenieros.com>
2016-03-08 01:06:11 -0700

    version terminada (todo bien)

--- app/src/main/java/com/example/nahim/eva1_practicaexamen_1/Principal.java
+++ app/src/main/java/com/example/nahim/eva1_practicaexamen_1/Principal.java
@@ -8,6 +8,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
@@ -16,7 +17,10 @@ public class Principal extends AppCompatActivity {
     CheckBox chkboxAzucar, chkboxCrema;
     Button btnTotal;
     EditText edtCantidad;
-    int precio, cantidad, precioxcantidad, preciototal;
+    int precio, cantidad, precioxcantidad, preciototal, casoCafe=0;
+    TextView txtDescripcion;
+    String descripcion;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +34,86 @@ public class Principal extends AppCompatActivity {
         chkboxCrema = (CheckBox) findViewById(R.id.chkbxCrema);
         btnTotal = (Button) findViewById(R.id.btnTotal);
         edtCantidad = (EditText) findViewById(R.id.edtCantidad);
+        txtDescripcion = (TextView) findViewById(R.id.txtVwDescripcionCompra);
 
 
     }
 
     public void ClickTotal(View v) {
-
-         if (rdbtnExpreso.isClickable() == true) {
-
-          precio = 40;
-          String cantidad1 = edtCantidad.getText().toString();
-          cantidad = Integer.parseInt(cantidad1);
-           precioxcantidad = precio * cantidad;
-          if ((chkboxCrema.isClickable() == true) && (chkboxAzucar.isChecked() == true)) {
-              preciototal = precioxcantidad + 2;
-          } else if ((chkboxCrema.isClickable() == false) && (chkboxAzucar.isChecked() == false)) {
-              preciototal = precioxcantidad;
-          } else preciototal = precioxcantidad + 1;
-
-        } else if (rdbtnCapuccino.isClickable() == true) {
-          precio = 48;
-          cantidad = Integer.parseInt(edtCantidad.getText().toString());
-          precioxcantidad = precio * cantidad;
-          if ((chkboxCrema.isClickable() == true) && (chkboxAzucar.isChecked() == true)) {
-              preciototal = precioxcantidad + 2;
-          } else if ((chkboxCrema.isClickable() == false) && (chkboxAzucar.isChecked() == false)) {
-            preciototal = precioxcantidad;
-          } else preciototal = precioxcantidad + 1;
-
-        } else if (rdbtnAmericano.isClickable() == true) {
-                  precio = 20;
-          cantidad = Integer.parseInt(edtCantidad.getText().toString());
-          precioxcantidad = precio * cantidad;
-          if ((chkboxCrema.isClickable() == true) && (chkboxAzucar.isChecked() == true)) {
-              preciototal = precioxcantidad + 2;
-          } else if ((chkboxCrema.isClickable() == false) && (chkboxAzucar.isChecked() == false)) {
-              preciototal = precioxcantidad;
-          } else preciototal = precioxcantidad + 1;
+        if (rdbtnAmericano.isChecked()){
+            casoCafe =1;
+            descripcion = (edtCantidad.getText().toString())+" cafe(s) Americano";
         }
-        Toast toast = Toast.makeText(this,"Precio total es igual:"+preciototal,Toast.LENGTH_SHORT);
-        toast.show();
+        else if (rdbtnCapuccino.isChecked()){
+            casoCafe =2;
+           descripcion = edtCantidad.getText().toString()+" cafe(s) Capuccino";
+        }
+        else if (rdbtnExpreso.isChecked()) {
+            casoCafe = 3;
+            descripcion = edtCantidad.getText().toString()+" cafe(s) Expreso";
+        }
+        if ((chkboxAzucar.isChecked())&&(chkboxCrema.isChecked()==false))
+            descripcion= descripcion+ ", con Azucar extra";
+        else if ((chkboxAzucar.isChecked()==false)&&(chkboxCrema.isChecked()==true))
+            descripcion= descripcion+", con Crema extra";
+
+        switch (casoCafe){
+            case 1:
+                precio =20;
+                cantidad = Integer.parseInt(edtCantidad.getText().toString());
+                precioxcantidad= precio*cantidad;
+                    if ((chkboxAzucar.isChecked())&&(chkboxCrema.isChecked())) {
+                        preciototal = precioxcantidad + 2;
+                        descripcion = descripcion + ", con Azucar y Crema extra";
+                    }
+                    else if ((chkboxCrema.isChecked()==false)&&(chkboxAzucar.isChecked()==false)){
+                        preciototal=precioxcantidad;
+                        descripcion = descripcion+ ", sin ningun extra";
+                    }
+                    else
+                        preciototal = precioxcantidad+1;
+                txtDescripcion.setText(descripcion);
+                 toast = Toast.makeText(this,"Precio total es igual:"+preciototal,Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case 2:
+                precio =48;
+                cantidad = Integer.parseInt(edtCantidad.getText().toString());
+                precioxcantidad= precio*cantidad;
+                if ((chkboxAzucar.isChecked())&&(chkboxCrema.isChecked())) {
+                    preciototal = precioxcantidad + 2;
+                    descripcion = descripcion + ", con Azucar y Crema extra";
+                }
+                else if ((chkboxCrema.isChecked()==false)&&(chkboxAzucar.isChecked()==false)){
+                    preciototal=precioxcantidad;
+                    descripcion = descripcion+ ", sin ningun extra";
+                }
+                else
+                    preciototal = precioxcantidad+1;
+                txtDescripcion.setText(descripcion);
+                toast = Toast.makeText(this,"Precio total es igual:"+preciototal,Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case 3:
+                precio =30;
+                cantidad = Integer.parseInt(edtCantidad.getText().toString());
+                precioxcantidad= precio*cantidad;
+                if ((chkboxAzucar.isChecked())&&(chkboxCrema.isChecked())){
+                    preciototal=precioxcantidad+2;
+                    descripcion = descripcion + ", con Azucar y Crema extra";
+                }
+                else if ((chkboxCrema.isChecked()==false)&&(chkboxAzucar.isChecked()==false)){
+                    preciototal=precioxcantidad;
+                    descripcion = descripcion+ ", sin ningun extra";
+                }
+                else
+                    preciototal = precioxcantidad+1;
+                txtDescripcion.setText(descripcion);
+                toast = Toast.makeText(this,"Precio total es igual:"+preciototal,Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+
+        }
+
     }
 }
